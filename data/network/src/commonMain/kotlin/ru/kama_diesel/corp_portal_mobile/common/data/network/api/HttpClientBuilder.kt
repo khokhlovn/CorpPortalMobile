cd452563d259a805f68cd44f5fe86316311be787
Хохлов Никита Сергеевич <n.kho@skbkontur.ru>
2025-10-22 09:51:54 +0300

    Фикс warning-а

--- data/network/src/commonMain/kotlin/ru/kama_diesel/corp_portal_mobile/common/data/network/api/HttpClientBuilder.kt
+++ data/network/src/commonMain/kotlin/ru/kama_diesel/corp_portal_mobile/common/data/network/api/HttpClientBuilder.kt
@@ -2,6 +2,7 @@ package ru.kama_diesel.corp_portal_mobile.common.data.network.api
 
 import io.ktor.client.*
 
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 expect object HttpClientBuilder {
     fun build(): HttpClient
 }
